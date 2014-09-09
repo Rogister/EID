5f9b5a4a1482a26771a2864c778a00650b0bc0b5
Alain Rogister <unknown-user@10.20.60.110>
2014-09-09 16:29:17 +0200

    Archivage initial du module eID

---
+++ eIDViewController.cs
@@ -0,0 +1,259 @@
+using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using FreedelitySmartCard;
+using System.Threading;
+
+
+namespace eID
+{
+	public partial class eIDViewController : UIViewController
+    {
+        FIDSmartCardReader eID;
+        UIAlertView msg;
+		FIDStatusCode status;
+		FIDStatusCode statusSign;
+		NSMutableData Identity;
+		NSMutableData Adresse,digest;
+		NSData nsTexte;
+		Thread thr;
+
+        public eIDViewController () : base ("eIDViewController",null)
+        {
+        }
+        
+        public override void DidReceiveMemoryWarning ()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning ();
+            
+            // Release any cached data, images, etc that aren't in use.
+        }
+        
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+
+			try
+{
+			eID = new FIDSmartCardReader();
+			}
+			catch
+			{
+
+			}
+
+
+
+        }
+
+
+        partial void btLecture (MonoTouch.UIKit.UIButton sender)
+		{
+			LectureEID();
+		}
+
+        partial void btSignature (MonoTouch.UIKit.UIButton sender)
+        {
+			thr = new Thread( SignatureEID);
+
+			thr.Start();
+
+            //SignatureEID();
+        }
+
+		void LectureEID()
+        {
+          
+            FIDSmartCard card;
+			FIDBelgianEidCard beid;
+
+
+			string strNom = "";
+			string strAdresse = "";
+			Identity = new NSMutableData();
+			Adresse = new NSMutableData();
+
+
+      
+
+            if (eID != null)
+            {
+				status = eID.Open();
+
+				if (status == FIDStatusCode.OK)
+				{
+
+					card = new FIDSmartCard();
+
+					card = eID.GetCard(status,true);
+
+					if (status == FIDStatusCode.OK)
+					{
+						beid = new FIDBelgianEidCard(card);
+
+						beid.ReadIdentityFile(Identity);
+
+						beid.ReadAddressFile(Adresse);
+
+						strNom = FIDBelgianEidCard.GetLastNameFromID((NSData)Identity);
+
+						strAdresse = FIDBelgianEidCard.GetStreetAndNumberFromAddress((NSData)Adresse) + " " +  FIDBelgianEidCard.GetMunicipalityFromAddress((NSData)Adresse);
+
+						msg = new UIAlertView() { Title = "MEDINECT", Message = strNom + " " + strAdresse };
+	                    msg.AddButton("OK");
+	                        
+	                           
+	                  	 msg.Show();
+					}
+
+				}
+
+				eID.Close();
+        
+            }
+
+        }
+
+        void SignatureEID()
+        {
+          
+            FIDSmartCard card;
+			FIDBelgianEidCard beid;
+			FIDPinDialogDescription pinDialog;
+
+			string strNom = "";
+			string strAdresse = "";
+			Identity = new NSMutableData();
+			Adresse = new NSMutableData();
+			digest = new NSMutableData();
+			int intEssai = 0;
+			NSData strSignature;
+
+
+            if (eID != null)
+            {
+				status = eID.Open();
+
+				if (status == FIDStatusCode.OK)
+				{
+					statusSign = FIDStatusCode.WRONG_PIN;
+
+					card = new FIDSmartCard();
+
+					card = eID.GetCard(status,true);
+
+					if (status == FIDStatusCode.OK)
+					{
+						beid = new FIDBelgianEidCard(card);
+
+						nsTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+						pinDialog = new FIDPinDialogDescription();
+
+						pinDialog.Lang = FIDPinDialogLang.French;
+						pinDialog.PinMaxLength = 4;
+						pinDialog.PinMinLength = 4;
+						//pinDialog.Controller = this;
+
+						strSignature = beid.SignData(nsTexte,FIDHashMethod.SHA1, ref statusSign, ref intEssai,digest,pinDialog);
+
+						InvokeOnMainThread (delegate {
+
+							if (statusSign == FIDStatusCode.OK)
+							{
+								msg = new UIAlertView() { Title = "MEDINECT", Message = strSignature.ToString() };
+	                    		msg.AddButton("OK");
+	                        
+	                           
+	                  			 msg.Show();
+							}
+							else
+							{
+								msg = new UIAlertView() { Title = "MEDINECT", Message = "Erreur : " + intEssai };
+	                    		msg.AddButton("OK");
+	                        
+	                           
+	                  			 msg.Show();
+							}
+						});
+					}
+
+				}
+
+				eID.Close();
+        
+            }
+
+        }
+
+     
+
+//		public class eIDEvent : eidEventsDelegate
+//        {
+//            UIAlertView msg;
+//		
+//
+//            public override void EIDeVent (string type, string Message)
+//            {
+//                base.EIDeVent (type, Message);
+//
+//                if (type == "SUCCESS")
+//                {
+//                    //msg = new UIAlertView() { Title = "MEDINECT", Message = eID.GetSignature().ToString() };
+//                    msg.AddButton("OK");
+//                        
+//                            
+//                    msg.Show();
+//                }
+//
+//                if (type == "FAILURE")
+//                {
+//                    msg = new UIAlertView() { Title = "MEDINECT", Message = "Problème de signature" };
+//                    msg.AddButton("OK");
+//                        
+//                            
+//                    msg.Show();
+//                }
+//
+//                if (type == "BAD_PIN")
+//                {
+//                    msg = new UIAlertView() { Title = "MEDINECT", Message = "Mauvais code PIN" };
+//                    msg.AddButton("OK");
+//                        
+//                            
+//                    msg.Show();
+//                }
+//            }
+//
+//		
+//        }
+//
+//		public class eIDPIN : eidPinDelegate
+//        {
+//           	
+//            public override bool UseDefaultDialog {
+//                get {
+//                    return true;
+//                }
+//            }
+//
+//            public override string DefaultDialogLang {
+//                get {
+//                    return "fr";
+//                }
+//            }
+//
+//            public override string AskPinCode {
+//                get {
+//                    return "";
+//                }
+//            }
+//
+//        }
+         
+    }
+
+}
+
---
+++ eIDViewController.designer.cs
@@ -0,0 +1,25 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+
+namespace eID
+{
+	[Register ("eIDViewController")]
+	partial class eIDViewController
+	{
+		[Action ("btLecture:")]
+		partial void btLecture (MonoTouch.UIKit.UIButton sender);
+
+		[Action ("btSignature:")]
+		partial void btSignature (MonoTouch.UIKit.UIButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
